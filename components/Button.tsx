2d92ea9363fc2eaeec9cbc9ef8e986b88975541f
jonaszbil <jonasz.bilinski@davra.com>
2023-03-02 09:51:29 +0000

    Configure staging deployment

--- components/Button.tsx
+++ components/Button.tsx
@@ -4,7 +4,7 @@ const Button: FC<React.ComponentProps<"button">> = (props) => {
   const { className, children, ...restProps } = props;
   return (
     <button
-      className={`w-40 rounded-full border-2 border-white bg-white py-3 px-8 font-lexend text-black mix-blend-lighten transition-all hover:bg-transparent hover:text-white ${className}`}
+      className={`w-40 rounded-full border-2 border-white bg-white py-3 px-8 font-lexend text-black mix-blend-lighten transition-all hover:bg-transparent hover:text-white ${className ?? ''}`}
       {...restProps}
     >
       {children}
