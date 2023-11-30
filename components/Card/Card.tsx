ba2bf2d32995d7d70447d8f5681529c438a6e8e5
jonaszbil <jonasz.bilinski@davra.com>
2023-11-30 16:19:43 +0000

    Change letter spacing

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -8,7 +8,7 @@ const Card: FC<{ title: string; text: string; color: string }> = ({ title, text,
     <div data-testid="card" className={`${color} h-[32rem] w-80 p-12 text-white flex flex-col justify-between`}>
       <div>
         <div className="flex justify-center items-center mb-9 aspect-square w-10 rounded-full bg-black bg-opacity-20"/>
-        <h2 className="mb-6 font-big-shoulders text-5xl font-bold tracking-wider">{title}</h2>
+        <h2 className="mb-6 font-big-shoulders text-5xl font-bold tracking-wide">{title}</h2>
         <p data-testid='card-text' className="font-lexend text-[14px] leading-6 opacity-75">{text}</p>
       </div>
       <Button onClick={() => setWasClicked(prev => !prev)}>{wasClicked ? 'Clicked' : 'Click me'}</Button>
